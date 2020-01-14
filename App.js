158ec34aa5bcf7b65029adc6da2bf5cfa39de490
villelie <ville.liekari@metropolia.fi>
2020-01-14 14:26:06 +0200

    init

---
+++ App.js
@@ -0,0 +1,84 @@
+import React from 'react';
+import {StyleSheet, Text, View, FlatList, Image, TouchableOpacity} from 'react-native';
+
+const mediaArray = [
+  {
+    'key': '0',
+    'title': 'Title 1',
+    'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sodales enim eget leo condimentum vulputate. Sed lacinia consectetur fermentum. Vestibulum lobortis purus id nisi mattis posuere. Praesent sagittis justo quis nibh ullamcorper, eget elementum lorem consectetur. Pellentesque eu consequat justo, eu sodales eros.',
+    'thumbnails': {
+      w160: 'http://placekitten.com/160/161',
+    },
+    'filename': 'http://placekitten.com/2048/1920',
+  },
+  {
+    'key': '1',
+    'title': 'Title 2',
+    'description': 'Donec dignissim tincidunt nisl, non scelerisque massa pharetra ut. Sed vel velit ante. Aenean quis viverra magna. Praesent eget cursus urna. Ut rhoncus interdum dolor non tincidunt. Sed vehicula consequat facilisis. Pellentesque pulvinar sem nisl, ac vestibulum erat rhoncus id. Vestibulum tincidunt sapien eu ipsum tincidunt pulvinar. ',
+    'thumbnails': {
+      w160: 'http://placekitten.com/160/162',
+    },
+    'filename': 'http://placekitten.com/2041/1922',
+  },
+  {
+    'key': '2',
+    'title': 'Title 3',
+    'description': 'Phasellus imperdiet nunc tincidunt molestie vestibulum. Donec dictum suscipit nibh. Sed vel velit ante. Aenean quis viverra magna. Praesent eget cursus urna. Ut rhoncus interdum dolor non tincidunt. Sed vehicula consequat facilisis. Pellentesque pulvinar sem nisl, ac vestibulum erat rhoncus id. ',
+    'thumbnails': {
+      w160: 'http://placekitten.com/160/163',
+    },
+    'filename': 'http://placekitten.com/2039/1920',
+  },
+];
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={mediaArray}
+        renderItem={({item}) => {
+          return (
+            <TouchableOpacity style={styles.flex}>
+              <Image
+                style={styles.image}
+                source={{uri: item.thumbnails.w160}}
+              />
+              <View style={styles.text}>
+                <Text style={{fontSize: '1.2em', fontWeight: 'bold'}}>{item.title}</Text>
+                <Text>{item.description}</Text>
+              </View>
+            </TouchableOpacity>
+          );
+        }}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    fontSize: '1em',
+  },
+  flex: {
+    backgroundColor: '#999',
+    flex: 1,
+    flexDirection: 'row',
+    marginBottom: 5,
+  },
+  image: {
+    flexGrow: 1,
+    minWidth: 160,
+    minHeight: 160,
+    margin: 5,
+  },
+  text: {
+    flexShrink: 1,
+    margin: 5,
+  },
+});
+
+export default App;
