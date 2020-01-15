5ecc6562c5a2fb36e386df04b53af94c0ef3dc80
villelie <ville.liekari@metropolia.fi>
2020-01-15 10:10:51 +0200

    AJAX + state

--- components/List.js
+++ components/List.js
@@ -1,20 +1,16 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import {FlatList} from 'react-native';
-import ListItem from './ListItem.js';
-import PropTypes from 'prop-types';
+import ListItem from './ListItem';
+import {MediaContext} from '../contexts/MediaContext';
 
 const List = (props) => {
-    console.log(props);
+    const [media] = useContext(MediaContext);
     return (
         <FlatList
-            data={props.mediaArray}
+            data={media}
             renderItem={({item}) => <ListItem singleMedia={item} />}
         />
     );
-};
-
-List.propTypes = {
-    mediaArray: PropTypes.array,
 };
 
 export default List;
